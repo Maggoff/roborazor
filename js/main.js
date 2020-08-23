ad08ebb4338d1001f5e20c4d1e9f8dad479b2825
Maggoff <magmbov@gmail.com>
2020-08-23 08:54:50 +0300

    Доробив адаптацію на мобілки

--- js/main.js
+++ js/main.js
@@ -1,14 +1,27 @@
-$(document).ready(function(){
-  $("#menu, #menu-footer, #main-btn").on("click","a", function (event) {
-      event.preventDefault();
-      var id  = $(this).attr('href'),
-          top = $(id).offset().top;
-      $('body,html').animate({scrollTop: top}, 600);
+$(document).ready(function () {
+  $("#menu").on("click", "a", function (event) {
+    event.preventDefault();
+    var id = $(this).attr('href'),
+      top = $(id).offset().top;
+    setTimeout(scroll(), 800);
+    var x = document.getElementById("links");
+    x.classList.toggle("show");
+    document.body.classList.toggle("body__overflow");
+    function scroll(){
+      $('body,html').animate({ scrollTop: top }, 600);
+    };
+  });
+
+  $("#menu-footer, #main-btn").on("click", "a", function (event) {
+    event.preventDefault();
+    var id = $(this).attr('href'),
+      top = $(id).offset().top;
+    $('body,html').animate({ scrollTop: top }, 600);
   });
 });
 
 function myFunction() {
   var x = document.getElementById("links");
   x.classList.toggle("show");
-  document.body.classList.toggle("body__overfloy");
+  document.body.classList.toggle("body__overflow");
 }
